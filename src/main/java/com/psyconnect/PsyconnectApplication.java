1523f4cb41e4cd1e9fc93694624c800d7142e6b1
Kaue Sousa <kauelimasousa02@gmail.com>
2024-09-18 21:24:01 -0300

    Projeto criado

---
+++ src/main/java/com/psyconnect/PsyconnectApplication.java
@@ -0,0 +1,13 @@
+package com.psyconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsyconnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsyconnectApplication.class, args);
+	}
+
+}
